b6ad0f9ed8ac67b499fb98ae9b21fe082f50e247
Laura Court <laura.mcourt@gmail.com>
2021-08-11 12:56:15 -0400

    Bid methods minus close_bidding

--- lib/item.rb
+++ lib/item.rb
@@ -5,4 +5,20 @@ class Item
     @name = name
     @bids = Hash.new(0)
   end
+
+  def add_bid(person, amt)
+    @bids[person] = amt
+  end
+
+  def current_high_bid
+    @bids.values.max
+  end
+
+
+  #this was frustrating.
+  def close_bidding
+    @bids.reject! do |a, bid|
+      add_bid(a, bid)
+    end
+  end
 end
