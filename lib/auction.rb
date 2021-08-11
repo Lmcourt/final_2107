9133e79f45843441ad0169c8b589795b2908fc51
Laura Court <laura.mcourt@gmail.com>
2021-08-11 12:56:36 -0400

    Bidding added

--- lib/auction.rb
+++ lib/auction.rb
@@ -4,4 +4,49 @@ class Auction
   def initialize
     @items = []
   end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def item_names
+    @items.map do |item|
+      item.name
+    end
+  end
+
+  def unpopular_items
+    @items.find_all do |item|
+      item.bids == {}
+    end
+  end
+
+  def potential_revenue
+    @items.sum do |item|
+      item.current_high_bid.to_i
+    end
+  end
+
+  def bidders
+    @items.flat_map do |item|
+      item.bids.map do |attendee, amt|
+        attendee.name
+      end
+    end.uniq
+  end
+
+  def bidder_info
+    info = Hash.new
+    @items.each do |item|
+      item.bids.find_all do |attendee, amt|
+        if info[attendee].nil?
+          info[attendee] = {budget: attendee.budget, items: [item]}
+        else
+          info[attendee][:budget] = attendee.budget
+          info[attendee][:items] << item
+        end
+      end
+    end
+    info
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -8,4 +8,142 @@ RSpec.describe Auction do
     expect(auction).to be_a(Auction)
     expect(auction.items).to eq([])
   end
+
+  it 'has items to auction off' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    expect(auction.items). to eq([item1, item2])
+  end
+
+  it 'has item names' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
+
+  it 'has an unpopular item' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+
+    expect(auction.unpopular_items).to eq([item2, item3, item5])
+
+    item3.add_bid(attendee2, 15)
+
+    expect(auction.unpopular_items).to eq([item2, item5])
+
+  end
+
+  it 'has potential revenue' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    expect(auction.potential_revenue).to eq(87)
+  end
+
+  it 'has bidder names' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    expect(auction.bidders).to eq(["Megan", "Bob", "Mike"])
+  end
+
+  it 'has bidder info' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    expected = ({
+                attendee1 => {:budget => 50, :items => [item1]},
+                attendee2 => {:budget => 75, :items => [item1, item3]},
+                attendee3 => {:budget => 100, :items => [item4]}
+                })
+
+    expect(auction.bidder_info).to eq(expected)
+  end
 end
