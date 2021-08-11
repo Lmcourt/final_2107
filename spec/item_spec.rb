75b24c203173f474591c69a279e98fe3a07d15ee
Laura Court <laura.mcourt@gmail.com>
2021-08-11 11:09:10 -0400

    Initialize

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  it 'exists with attrinutes' do
+    item1 = Item.new('Chalkware Piggy Bank')
+
+    expect(item1).to be_a(Item)
+    expect(item1.name).to eq("Chalkware Piggy Bank")
+    expect(item1.bids).to eq({})
+  end
+end
